1232054987ab21695b01867e7a56d48b5ad1f8c2
Joshua Smith <jjsmith260@hotmail.com>
2018-07-11 06:28:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysto(name)  {
-  return "Happy holidays, ${name}!"
+function happyHolidaysTo(name)  {
+  return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysto(holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days)  {
-  return "It's ${days} days until ${holday;}!"
+  return `It's ${days} days until ${holday}!`
 }
